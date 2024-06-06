d07b161c8df7923a94e73ea7ce759cdb06a07bd6
jobjohnek2020 <78946922+jobjohnek2020@users.noreply.github.com>
2024-06-06 20:21:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ st.set_page_config(page_title='AIDS Virus Infection Prediction')
 st.title('AIDS Virus Infection Prediction')
 
 with st.expander('About'):
-  st.info(f'Used dataset in [link](%s) to predict AIDS infection','https://www.kaggle.com/datasets/aadarshvelu/aids-virus-infection-prediction')
-  
+  st.info('Used dataset in kaggle to predict AIDS infection')
+  st.link_button("AIDS Infection Prediction", 'https://www.kaggle.com/datasets/aadarshvelu/aids-virus-infection-prediction')
   st.markdown('Libraries used:')
   st.code('''- Pandas for data wrangling
 - Scikit-learn for building a machine learning model
