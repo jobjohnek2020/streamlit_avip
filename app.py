ff7ebb04c483dbd5d1944aaa7932e49d4dcecec4
jobjohnek2020 <78946922+jobjohnek2020@users.noreply.github.com>
2024-06-06 19:36:26 +0530

    Update app.py
    Added training dataset in UI

--- app.py
+++ app.py
@@ -20,22 +20,28 @@ with st.expander('About'):
 - Scikit-learn for building a machine learning model
 - Streamlit for user interface
   ''', language='markdown')
+
 # reading dataset
 df = pd.read_csv('dataset/AIDS_Classification_50000.csv')
+
 # Number of input features
 x_variables = len(df.columns) - 1
+
 # removing outliers
 Q1 = df.quantile(q=0.25)
 Q3 = df.quantile(q=0.75)
 IQR = df.apply(stats.iqr)
 data_clean = df[~((df < (Q1-1.5*IQR)) | (df > (Q3+1.5*IQR))).any(axis=1)]
+
 # removing skewness
 x = data_clean.iloc[:,:-1]
 y = data_clean.iloc[:,-1].values
 quantile_transformer = QuantileTransformer(output_distribution='normal', random_state=1)
 x_t = quantile_transformer.fit_transform(x.values)
+
 # splitting data as training and testing data
 x_train,x_test,y_train,y_test = train_test_split(x_t,y,test_size=0.30,random_state=1)
+
 # setting input data in UI
 st.header('Input data', divider='rainbow')
 col = st.columns(4)
@@ -44,8 +50,16 @@ col[1].metric(label='No of X variables',value=x_variables,delta='')
 col[2].metric(label='No of training samples',value=x_train.shape[0],delta='')
 col[3].metric(label='No of testing samples',value=x_test.shape[0],delta='')
 
-# initial dataset
+# setting initial dataset in UI
 with st.expander('Initial dataset',expanded=True):
   st.dataframe(df.head(10),height=210,use_container_width=True)
 
-
+# setting training dataset
+with st.expander('Training split',expander=True):
+  train_col = st.columns((3,1))
+  with train_col[0]:
+    st.markdown('>>X<<')
+    st.dataframe(x_train,height=210,use_container_width=True)
+  with train_col[1]:
+    st.markdown('>>Y<<')
+    st.dataframe(y_train,height=210,use_container_width=True)
